2b6ed65a5b491993eb90114b989976ee2bf05f7f
David Hunt <huntdog1541@gmail.com>
2016-01-03 05:14:55 -0600

    updated code

--- js/main.js
+++ js/main.js
@@ -3,5 +3,50 @@
  */
 
 window.onload =  function addRoot() {
-    document.getElementById("text").value = "root$ ";
+    var x = document.getElementById("text");
+    x.value = "root$ ";
 };
+/*
+window.onclick = function clicked() {
+    document.getElementById("display").value = "clicked!";
+};*/
+
+var console = {
+    startLoc: null,
+    endLoc: 0,
+    con: document.getElementById("text"),
+    dis: document.getElementById("display"),
+    history: 0,
+    histCommands: []
+};
+
+
+
+function getStart() {
+    if(console.startLoc == null)
+    {
+        console.startLoc = console.con.selectionStart
+    }
+    document.getElementById("display").value = console.startLoc;
+}
+
+function getKeys() {
+    //disableConsole();
+    $('#text').keypress(function() {
+       $('#display').value += "  detected keypress";
+    });
+}
+
+function disableConsole() {
+    console.con.readOnly = true;
+}
+
+function enableConsole() {
+    console.con.readOnly = false;
+}
+
+function typing() {
+    getStart();
+    //getKeys();
+}
+console.con.onclick = typing();
